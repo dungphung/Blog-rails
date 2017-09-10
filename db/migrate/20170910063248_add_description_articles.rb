936889168ec632874287560929207492b5c07c81
pvDung <dungvatoi12@gmail.com>
2017-09-10 13:53:28 +0700

    Create articles table, model and add validations

---
+++ db/migrate/20170910063248_add_description_articles.rb
@@ -0,0 +1,8 @@
+class AddDescriptionArticles < ActiveRecord::Migration[5.0]
+  def change
+    add_column :articles, :description, :text
+    add_column :articles, :create_at, :datetime
+    add_column :articles, :update_at, :datetime
+    
+  end
+end
